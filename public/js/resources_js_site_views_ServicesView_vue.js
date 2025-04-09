fb3daf66e96ba2b5a19bd547719708b6568bdb43
mariechristsagbo <christsagbo1@gmail.com>
2025-04-09 19:01:59 +0100

    refactor: suppression des services de CEV et de signature électronique

---
+++ public/js/resources_js_site_views_ServicesView_vue.js
@@ -0,0 +1,65 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_site_views_ServicesView_vue"],{
+
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   render: () => (/* binding */ render)
+/* harmony export */ });
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
+
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"breadcrumb-area\" style=\"background-image:url(assets/img/page-title-bg.png);\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12\"><div class=\"breadcrumb-inner\"><h1 class=\"page-title\">Services</h1><ul class=\"page-list\"><li><a href=\"/\">Accueil</a></li><li>Nos services</li></ul></div></div></div></div></div>", 1);
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"sbst-service-area pd-top-112\"><div class=\"container\"><div class=\"row justify-content-center\"><div class=\"col-xl-6 col-lg-10\"><div class=\"section-title text-center\"><h2 class=\"title\">Nos <span>services</span></h2><p> Nous vous offrons des services d&#39;intégration de solutions numériques de diverses catégories, des solutions de Sécurité informatiques, des conseils et formations. </p></div></div></div></div></div>", 1);
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"work-processing-area pd-top-1\"><div class=\"container\"><div class=\"row justify-content-center custom-gutters-16 single-work-processing\"><div class=\"col-xl-5 col-md-6 order-lg-12\"><div class=\"thumb swp-right-thumb\"><img src=\"assets/img/work-processing/solution.jpeg\" alt=\"img\"></div></div><div class=\"col-xl-4 col-md-6 order-lg-1 desktop-center-item\"><div class=\"work-processing-details\"><div class=\"section-title style-four\"><h1 class=\"counting-number\">1</h1><h2 class=\"title\">Intégration de <span>Solutions</span></h2><p> Nous intégrons des solutions numériques de diverses catégories cités ci-dessous: <br></p><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Solutions de confiance numérique</span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Solutions intelligentes</span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Intégration de solutions cloud</span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Intégration de solutions maritimes </span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Gestion électronique de courriers et de documents. </span></div></div></div></div></div><div class=\"row justify-content-center custom-gutters-16 single-work-processing\"><div class=\"col-xl-5 col-md-6\"><div class=\"thumb\"><img src=\"assets/img/work-processing/security.jpg\" alt=\"img\"></div></div><div class=\"col-xl-4 col-md-6 desktop-center-item\"><div class=\"work-processing-details\"><div class=\"section-title style-four\"><h1 class=\"counting-number\">2</h1><h2 class=\"title\"><span>Solutions de </span> Sécurité</h2><p> Les solutions de sécurité informatiques ne sont pas en marge de notre zone de confort. Nous intervenons dans les catégories suivantes: </p><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Analyste SOC</span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Intégrateur de solutions de sécurité</span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Système de supervision </span></div><div class=\"riyaqas-check-list\"><img src=\"assets/img/icons/check2.svg\" alt=\"check\"><span>Pentesting (Tests d&#39;intrusion).</span></div></div></div></div></div><div class=\"row justify-content-center custom-gutters-16 single-work-processing\"><div class=\"col-xl-5 col-md-6 order-lg-12\"><div class=\"thumb swp-right-thumb\"><img src=\"assets/img/work-processing/formation.jpeg\" alt=\"img\"></div></div><div class=\"col-xl-4 col-md-6 order-lg-1 desktop-center-item\"><div class=\"work-processing-details\"><div class=\"section-title style-four\"><h1 class=\"counting-number\">3</h1><h2 class=\"title\">Conseils et <span>Formations</span></h2><p> Nous offrons des formations relatives à nos domaines d&#39;interventions pour permettre à nos partenaires de mieux jouir des solutions que nous mettons à leur dispositions. </p></div></div></div></div></div></div>", 1);
+function render(_ctx, _cache) {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" breadcrumb area start "), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" breadcrumb area End "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" sbst service area start "), _hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" sbst service area end "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Ui element start "), _hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Ui element End ")], 64 /* STABLE_FRAGMENT */);
+}
+
+/***/ }),
+
+/***/ "./resources/js/site/views/ServicesView.vue":
+/*!**************************************************!*\
+  !*** ./resources/js/site/views/ServicesView.vue ***!
+  \**************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _ServicesView_vue_vue_type_template_id_2ce711fe__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ServicesView.vue?vue&type=template&id=2ce711fe */ "./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe");
+/* harmony import */ var _node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+
+const script = {}
+
+;
+const __exports__ = /*#__PURE__*/(0,_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__["default"])(script, [['render',_ServicesView_vue_vue_type_template_id_2ce711fe__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/site/views/ServicesView.vue"]])
+/* hot reload */
+if (false) {}
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__exports__);
+
+/***/ }),
+
+/***/ "./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe":
+/*!********************************************************************************!*\
+  !*** ./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe ***!
+  \********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   render: () => (/* reexport safe */ _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_ServicesView_vue_vue_type_template_id_2ce711fe__WEBPACK_IMPORTED_MODULE_0__.render)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_use_0_node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_ServicesView_vue_vue_type_template_id_2ce711fe__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!../../../../node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!../../../../node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./ServicesView.vue?vue&type=template&id=2ce711fe */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/js/site/views/ServicesView.vue?vue&type=template&id=2ce711fe");
+
+
+/***/ })
+
+}]);
--- resources/js/site/components/services/GestionImmobiliereComponent.vue
+++ resources/js/site/components/services/GestionImmobiliereComponent.vue
@@ -6,7 +6,7 @@
                 <div class="col-lg-4">
                     <div class="work-processing-details">
                         <div class="section-title style-four">
-                            <h1 class="counting-number">4</h1>
+                            <h1 class="counting-number">2</h1>
                             <h2 class="title">
                                 Solution de <span>gestion immobilière </span>
                             </h2>
